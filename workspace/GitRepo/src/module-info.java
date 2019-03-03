bd1dda96344118987649c3707b1739352cf8ec2d
sudheer <sudheer.java.mean@gmail.com>
2019-03-03 13:44:12 +0530

    s commit

---
+++ workspace/GitRepo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author welcome
+ *
+ */
+module GitRepo {
+}
---
+++ workspace/GitRepo/src/s/io/ExGit.java
@@ -0,0 +1,15 @@
+package s.io;
+
+public class ExGit {
+
+	public ExGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi...");
+
+	}
+
+}
